4712187af9f93ae0e5ce9d664d0fbc47078c7ab5
AnhMinh <minhanh@beeknight.com>
2023-11-20 11:43:40 +0700

    build: depenancy lib

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,11 @@
+export const TopBar = () => {
+  return (
+    <div>
+      <img
+        src="/src/images/logo.svg"
+        alt="logo"
+        className="!w-[20px] !h-[20px] bg-[red]"
+      />
+    </div>
+  );
+};
